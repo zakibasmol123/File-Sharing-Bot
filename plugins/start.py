5fa66995ee9e4760c5d3dc1ee3bdcbb0733f1800
OnlyMidnight <87945444+OnlyMidnight@users.noreply.github.com>
2021-08-01 01:26:16 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -40,11 +40,11 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("**Tunggu Sebentar**...")
         try:
             messages = await get_messages(client, ids)
         except:
-            await message.reply_text("Something went wrong..!")
+            await message.reply_text("**Sedang Mengalami Kendala**..!")
             return
         await temp_msg.delete()
 
